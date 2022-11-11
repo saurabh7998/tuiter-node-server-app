a4ad12b1e8915d48be61db6635954196ac0f10d1
Saurabh Gade <saurabhg@saurabhs-air.myfiosgateway.com>
2022-11-10 22:48:02 -0500

    sets up tuiter server

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,43 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+
+const TuitsController = (app) => {
+    app.get('/api/tuits', getTuits)
+    app.post('/api/tuits', createTuit)
+    app.delete('/api/tuits/:tid', deleteTuit)
+    app.put('/api/tuits/:tid', updateTuit);
+}
+
+const deleteTuit = (req, res) => {
+    const tuitId = req.params['tid']
+    tuits = tuits.filter((tuit) => tuit._id !== tuitId)
+    res.sendStatus(200);
+}
+
+const getTuits = (req, res) => {
+    res.json(tuits);
+}
+
+const createTuit = (req, res) => {
+    const newTuit = req.body
+    newTuit._id = (new Date()).getTime() + ''
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    tuits.push(newTuit)
+    res.json(newTuit)
+}
+
+const updateTuit = (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex(
+        (t) => t._id === tuitdIdToUpdate)
+    tuits[tuitIndex] =
+        {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+}
+
+
+
+export default TuitsController
---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        "avatarImage": "../../images/elon_musk.png",
+        "userName": "Elon Musk",
+        "handleName": "@elonmusk",
+        "time": "23h",
+        "topic": "Space",
+        "content": "Amazing show about @Inspiration4x mission",
+        "replies": 400,
+        "retuits": 300,
+        "likes": 370,
+        "liked": true,
+        "_id": "123"
+    },
+    {
+        "avatarImage": "../../images/newyorkpost.png",
+        "userName": "New York Post",
+        "handleName": "@nypost",
+        "time": "23h",
+        "topic": "Entertainment",
+        "content": "Grimes seen reading Karl Marx following split with world\"s richest man Elon Musk",
+        "replies": 965,
+        "retuits": 240,
+        "likes": 400,
+        "liked": false,
+        "_id": "234"
+    }
+]
